d53076bb130189f12dd7e5eb6292b7afde779ec3
mazlumkor <mazlumkor1@gmail.com>
2025-12-24 15:06:30 +0300

    Add abstract Question base class

---
+++ src/Question.java
@@ -0,0 +1,17 @@
+public abstract class Question {
+
+    protected String questionText;
+    protected String correctAnswer;
+
+    public Question(String questionText, String correctAnswer) {
+        this.questionText = questionText;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public abstract boolean checkAnswer(String userAnswer);
+}
+
