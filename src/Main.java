dd06dc9e669b8fd81dc110acbbe8317fb7123dc4
mazlumkor <mazlumkor1@gmail.com>
2025-12-26 23:45:47 +0300

    Add Main class and initialize quiz with 10 questions

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,67 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Quiz quiz = new Quiz();
+
+        // 5 True / False questions
+        quiz.addQuestion(new TrueFalseQuestion(
+                "Java is an object oriented programming language. (true/false)",
+                "true"
+        ));
+
+        quiz.addQuestion(new TrueFalseQuestion(
+                "A class can have multiple constructors. (true/false)",
+                "true"
+        ));
+
+        quiz.addQuestion(new TrueFalseQuestion(
+                "Inheritance is not related to OOP. (true/false)",
+                "false"
+        ));
+
+        quiz.addQuestion(new TrueFalseQuestion(
+                "The 'main' method is the entry point of a Java program. (true/false)",
+                "true"
+        ));
+
+        quiz.addQuestion(new TrueFalseQuestion(
+                "In Java, variables do not need a data type. (true/false)",
+                "false"
+        ));
+
+        // 5 Multiple Choice Questions
+        quiz.addQuestion(new MultipleChoiceQuestion(
+                "Which keyword is used to inherit a class in Java?",
+                "extends",
+                Arrays.asList("extends", "implements", "import", "inherit")
+        ));
+
+        quiz.addQuestion(new MultipleChoiceQuestion(
+                "Which of the following is NOT a Java OOP concept?",
+                "Compilation",
+                Arrays.asList("Polymorphism", "Encapsulation", "Inheritance", "Compilation")
+        ));
+
+        quiz.addQuestion(new MultipleChoiceQuestion(
+                "Which method is the entry point of a Java application?",
+                "main",
+                Arrays.asList("start", "run", "main", "execute")
+        ));
+
+        quiz.addQuestion(new MultipleChoiceQuestion(
+                "Which keyword is used to create an object?",
+                "new",
+                Arrays.asList("create", "class", "new", "object")
+        ));
+
+        quiz.addQuestion(new MultipleChoiceQuestion(
+                "Which one is used to hide data?",
+                "Encapsulation",
+                Arrays.asList("Encapsulation", "Polymorphism", "Abstraction", "Inheritance")
+        ));
+
+        quiz.start();
     }
 }
