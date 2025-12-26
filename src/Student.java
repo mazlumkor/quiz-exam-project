ee292cdffb9f1c4c958f977757bc7244e8b21b41
mazlumkor <mazlumkor1@gmail.com>
2025-12-27 00:00:24 +0300

    Add department field to Student class

--- src/Student.java
+++ src/Student.java
@@ -2,10 +2,12 @@ public class Student {
 
     private String name;
     private String studentId;
+    private String department;
 
-    public Student(String name, String studentId) {
+    public Student(String name, String studentId, String department) {
         this.name = name;
         this.studentId = studentId;
+        this.department = department;
     }
 
     public String getName() {
@@ -14,5 +16,9 @@ public class Student {
 
     public String getStudentId() {
         return studentId;
+    }
+
+    public String getDepartment() {
+        return department;
     }
 }
