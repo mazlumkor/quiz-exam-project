36c875b617789dd8ec66f9787b498385f818287a
mazlumkor <mazlumkor1@gmail.com>
2025-12-26 23:59:04 +0300

    Add Student class to store student information

---
+++ src/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+
+    private String name;
+    private String studentId;
+
+    public Student(String name, String studentId) {
+        this.name = name;
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+}
